fbd3514dfd715609fc5b83bc31cb01d31ec02cc9
vaidehinarola <147324124+vaidehinarola@users.noreply.github.com>
2024-10-07 12:27:00 +0530

    gych

---
+++ app/static/js/home.js
@@ -0,0 +1,10 @@
+// gsap.registerPlugin(ScrollTrigger);
+
+// gsap.from("img", {
+//   scale: 0.9,
+//   scrollTrigger: {
+//     trigger: "#image",
+//     scrub: true,
+//     end: "+=700"
+//   }
+// });
